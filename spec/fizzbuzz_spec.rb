880a030413d93f30afcd8b16b0f2f9063ca69679
Tim Jones <timjones10@hotmail.com>
2017-09-19 16:31:24 +0100

    7 tests passed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,7 +12,7 @@ describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
-  
+
   it 'returns "fizz" when passed a multiple of 3 < 1000' do
     (0..1000).each do |i|
       if i % 3 == 0 && i % 5 != 0
@@ -24,10 +24,24 @@ describe 'fizzbuzz' do
 it 'returns "fizzbuzz" when passed a multiple of 3 and 5 < 1000' do
   (0..1000).each do |i|
       if i % 3 == 0 && i % 5 == 0
-        p i
         expect(fizzbuzz(i)).to eq 'fizzbuzz'
       end
     end
   end
 
+it 'returns "buzz" when passed a multiple of 5 which is not a multiple of 3 < 1000' do
+    (0..1000).each do |i|
+      if i % 5 == 0 && i % 3 != 0
+        expect(fizzbuzz(i)).to eq 'buzz'
+      end
+    end
+  end
+
+it 'returns the number unchanged when passed a number that is neither a multiple of 3 or 5 < 1000' do
+  (0..1000).each do |i|
+    if i % 5 != 0 && i % 3 != 0
+      expect(fizzbuzz(i)).to eq i
+      end
+    end
+  end
 end
