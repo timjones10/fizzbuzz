2c1da99bdb2e2b34ca2d19dbc619f13ee095442f
Tim Jones <timjones10@hotmail.com>
2017-09-19 16:07:17 +0100

    Test 5 passed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,12 +4,15 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+  
   it 'returns "fizz" when passed a multiple of 3 < 1000' do
     (0..1000).each do |i|
       if i % 3 == 0 && i % 5 != 0
@@ -17,4 +20,14 @@ describe 'fizzbuzz' do
       end
     end
   end
+
+it 'returns "fizzbuzz" when passed a multiple of 3 and 5 < 1000' do
+  (0..1000).each do |i|
+      if i % 3 == 0 && i % 5 == 0
+        p i
+        expect(fizzbuzz(i)).to eq 'fizzbuzz'
+      end
+    end
+  end
+
 end
